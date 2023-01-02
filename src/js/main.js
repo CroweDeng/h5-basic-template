70c24d0709622bb6833cd4110c2655304fb83b07
crowe <crowe.deng@neutec.com.tw>
2023-01-02 17:36:34 +0800

    Feat: 調整變數寫法、main.js-setFontSize判斷、新增documentHeight判斷

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,37 @@
 //? set fontSize
-const setFontSize = () => {
+const setFontSize = (device, range) => {
+  // mobile range = 414;
+  // desktop range = 1200;
   let width = document.documentElement.clientWidth;
-  let range = 1200;
-  //判斷視窗寬度 調整font-size
-  if (width > range) {
-    let scale = width / (range / 16);
-    $("html").css("font-size", scale + "px");
-  } else {
-    $("html").css("font-size", "var(--base-font-size)");
+  switch (device) {
+    case "mobile":
+      //判斷視窗寬度 調整font-size
+      if (width > range) {
+        let scale = width / (range / 16);
+        $("html").css("font-size", scale + "px");
+      } else {
+        $("html").css("font-size", "16px");
+      }
+      break;
+    case "desktop":
+      //判斷視窗寬度 調整font-size
+      if (width >= 1920) {
+        $("html").css("font-size", "16px");
+      } else if (width > range) {
+        let scale = width / (range / 12);
+        $("html").css("font-size", scale + "px");
+      } else {
+        $("html").css("font-size", "10px");
+      }
+      break;
   }
+};
+
+//? src https://dev.to/nirazanbasnet/dont-use-100vh-for-mobile-responsive-3o97
+//? resize each device height
+const documentHeight = () => {
+  const doc = document.documentElement;
+  doc.style.setProperty("--doc-height", `${window.innerHeight}px`);
 };
 
 $(document).ready(function () {
@@ -16,6 +39,7 @@ $(document).ready(function () {
   //? set html base font size
   setFontSize();
   window.onresize = function () {
-    setFontSize();
+    setFontSize("desktop", "1200");
+    documentHeight();
   };
 });
