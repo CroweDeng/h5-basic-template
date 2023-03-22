fef1522d8de945ca5f77b2f8251cf50135959ca8
crowe <crowe.deng@neutec.com.tw>
2023-03-22 10:41:18 +0800

    Styles: adjust main.js(device range)

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,29 @@
 //? set fontSize
-const setFontSize = (device, range) => {
+const setFontSize = (device, range, maxRange) => {
   // mobile range = 414;
   // desktop range = 1200;
   let width = document.documentElement.clientWidth;
   switch (device) {
-    case "mobile":
+    case 'mobile':
       //判斷視窗寬度 調整font-size
-      if (width > range) {
+      if (width >= maxRange) {
+        $('html').css('font-size', '16px');
+      } else if (width > range) {
         let scale = width / (range / 16);
-        $("html").css("font-size", scale + "px");
-      } else {
-        $("html").css("font-size", "16px");
+        $('html').css('font-size', scale + 'px');
       }
       break;
-    case "desktop":
+    case 'desktop':
       //判斷視窗寬度 調整font-size
-      if (width >= 1920) {
-        $("html").css("font-size", "16px");
+      if (width >= maxRange) {
+        $('html').css('font-size', '24px');
       } else if (width > range) {
-        let scale = width / (range / 12);
-        $("html").css("font-size", scale + "px");
-      } else {
-        $("html").css("font-size", "16px");
-      }
+        let scale = width / (range / 16);
+        $('html').css('font-size', scale + 'px');
+      } 
       break;
     default :
-      $("html").css("font-size", "16px");
+      $('html').css('font-size', '16px');
       break;
   }
 };
@@ -34,19 +32,19 @@ const setFontSize = (device, range) => {
 //? resize each device height
 const documentHeight = () => {
   const doc = document.documentElement;
-  doc.style.setProperty("--doc-height", `${window.innerHeight}px`);
+  doc.style.setProperty('--doc-height', `${window.innerHeight}px`);
 };
 
 
 $(document).ready(function () {
-  console.log("Hello Main.js Here!");
+  console.log('Hello Main.js Here!');
   //? set html base font size
-  setFontSize();
+  setFontSize('desktop', '1200', '1920');
   documentHeight();
   //? onresize to reset setFontSize & documentHeight
   window.onresize = function () {
     //? desktop first
-    setFontSize("desktop", "1200");
+    setFontSize('desktop', '1200', '1920');
     documentHeight();
   };
 });
