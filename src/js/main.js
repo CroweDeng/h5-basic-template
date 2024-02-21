a58d128197c30eaf4832995a31cbe30f1d47b48e
crowe <crowe.deng@neutec.com.tw>
2024-02-21 17:32:05 +0800

    Chore: adjust stylelint.json and add some main.js function

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,29 @@
 //? src https://dev.to/nirazanbasnet/dont-use-100vh-for-mobile-responsive-3o97
-//? resize each device height
+//? resize each device height -> 適合mobile 使用
 const documentHeight = () => {
   const doc = document.documentElement;
   doc.style.setProperty('--doc-height', `${window.innerHeight}px`);
 };
+//? 在mobile可以判斷button active或hover狀態 -> 適合mobile 使用
+window.onload = function () {
+  if (/iP(hone|ad)/.test(window.navigator.userAgent)) {
+    document.body.addEventListener('touchstart', function () {}, false);
+  }
+};
 
+//? set fontSize -> 適合Web使用
+const setFontSize = () => {
+  let width = document.documentElement.clientWidth;
+  // console.log(width);
+  let range = 1440;
+  // 判斷視窗寬度 調整font-size
+  if (width > range) {
+    $('html').css('font-size', '16px');
+  } else {
+    let scale = width / (range / 16);
+    $('html').css('font-size', scale > 12 ? scale : '12px');
+  }
+};
 
 $(document).ready(function () {
   console.log('Hello Main.js Here!');
@@ -12,5 +31,6 @@ $(document).ready(function () {
   //? onresize to reset setFontSize & documentHeight
   window.onresize = function () {
     documentHeight();
+    setFontSize();
   };
 });
